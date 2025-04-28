fe21216265736425f7daed7d71cb3aa8ad2f8077
ShotaKitazawa <skitazawa1121@gmail.com>
2025-04-28 13:32:23 +0900

    update

--- hack/tools.go
+++ hack/tools.go
@@ -4,5 +4,5 @@ package hack
 
 import (
 	_ "github.com/golang/mock/mockgen"
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/golangci/golangci-lint/v2/cmd/golangci-lint"
 )
